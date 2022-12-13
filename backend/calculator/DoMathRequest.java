eab6b9d984beec0d90c164e7b1731917a2292eb2
kethia123 <kemykayigire@gmail.com>
2022-12-13 09:50:16 +0200

    Files needed

---
+++ backend/calculator/DoMathRequest.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class DoMathRequest {
+    private double operand1;
+    private double operand2;
+    private String operation;
+}
---
+++ backend/calculator/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class InvalidOperationException extends Exception {
+    public InvalidOperationException(String message){
+        super(message);
+    }
+}
